84afa580ff594fd0f89fd246a68be94ade342a31
Deepak0056 <94425277+Deepak0056@users.noreply.github.com>
2023-07-20 14:08:47 +0545

    Initial Setup✌️

---
+++ OnlineStore/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    // main container
+    <div className='nav w-full bg-red-300 h-[3rem] '>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
